30b409f3a60846d24914b0761246ef8982c65dcc
heikkiveikko <hlotvonen@gmail.com>
2023-12-20 11:10:12 +0200

    removed line

---
+++ build/precache-manifest.bc312d68d692736ef3ad7a884948376a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49419a1e41453f9b3bf2dae7530ca04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48bcb82ef1e19993aa42",
+    "url": "/static/css/main.49b46d74.chunk.css"
+  },
+  {
+    "revision": "02d4ee6a58c80d74781a",
+    "url": "/static/js/2.28d1b774.chunk.js"
+  },
+  {
+    "revision": "aaeff35d979defb99a763f8258984a23",
+    "url": "/static/js/2.28d1b774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48bcb82ef1e19993aa42",
+    "url": "/static/js/main.d6b3fd13.chunk.js"
+  },
+  {
+    "revision": "207d0ba08cb86645738c",
+    "url": "/static/js/runtime-main.322b43bb.js"
+  },
+  {
+    "revision": "a0cba6fc304081ce7e7e5c3bd16b1757",
+    "url": "/static/media/TopazPlusa1200v1.0.a0cba6fc.ttf"
+  },
+  {
+    "revision": "127d21b0213045f10e50b4ad09126da8",
+    "url": "/static/media/TopazPlusa600a1200a4000.127d21b0.woff2"
+  },
+  {
+    "revision": "2a4f8050f4fe157ff448391c9ba6c69b",
+    "url": "/static/media/TopazPlusa600a1200a4000.2a4f8050.woff"
+  }
+]);
